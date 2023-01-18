26c1a6c0d34da4137d3e8532b48650487e80bcc0
The-VoyZ <marco.goray@gmail.com>
2023-01-18 18:48:21 +0100

    Website V7 Added

---
+++ Helianas_Webpage_V7/MundaneMaterialCalculator.py
@@ -0,0 +1,41 @@
+from Monster_Dictionarys import region_selection , unrefined_dict
+
+class MundaneIngredients():
+    
+    #Calculation for the Return Materials: Units found = 5x(1+Check -DC) 
+    #DC Calculation = Region + Material Type
+    
+    def __init__(self,region,material, dc) -> None:
+        self.region = str(region)
+        self.material = str(material).lower()
+        self.material_dict = {}
+        self.DC = dc
+        self.material_units = 0
+        self.calculate()
+        
+    def material_DC(self):
+        self.material_dict = self.region_check()
+        if self.material in self.material_dict:
+            return self.material_dict[self.material]
+            
+    
+    
+    def region_check(self):
+        return region_selection[self.region]
+    
+    
+    def material_calulation(self):
+        if self.DC < int(self.material_DC()):
+            return 0
+        else:
+            self.material_units = 5 *(1 + self.DC - self.material_DC())
+        return int(self.material_units)
+   
+    def unrefined_materials(self):
+        unrefined_units = unrefined_dict[self.material]*self.material_units
+        return unrefined_units
+    
+    
+    def calculate(self):
+        
+        return ["Selected Region: "+ self.region.capitalize(), "Selected Material: " + self.material.capitalize(),"Dc =" + str(self.material_DC()), "Rolled DC:"+ str(self.DC), "You gethered " + str(self.material_calulation()) + " Units of " + self.material.capitalize() + " for a Total of " + str(self.unrefined_materials()) + " lbs"]
---
+++ Helianas_Webpage_V7/app.py
@@ -0,0 +1,90 @@
+from MonsterCalculator import HarvestCalculator
+from MundaneMaterialCalculator import MundaneIngredients
+from flask import Flask, render_template, request
+from Monster_Dictionarys import type_selection , region_selection , unrefined_dict
+app = Flask(__name__)
+
+
+
+@app.route('/')
+
+def index():
+    return render_template("index.html")
+
+@app.route('/calc', methods = ["POST"])
+def calc_mainpage():
+    options = type_selection
+    options2 ={}
+    return render_template("MonsterCalculator.html", options=options, options2 = options2)
+
+@app.route('/dict_selected', methods=["POST"])
+def dict_select():
+    monster_type = str(request.form["dropdown"])
+    selected_monster_type = {}
+    selected_monster_type[monster_type] = type_selection[monster_type]
+    options = selected_monster_type
+    options2 = type_selection[monster_type]
+    return render_template("MonsterCalculator.html", options=options, options2=options2 , monster_type=monster_type)
+
+@app.route('/calculate', methods=["POST"])
+def calculate():
+    monster_type = str(request.form["monster_type"])
+    # Abrufen der ausgewählten Option aus dem Dropdown-Menü
+    #monster_type = str(request.form["dropdown"])
+    # Abrufen der Zahl aus der Eingabe-Fläche
+    monster_CR = int(request.form["number"])
+    # Abrufen des Texts aus der Eingabe-Fläche
+   # monster_components = str(request.form["text"]).lower()
+    monster_components = str(request.form["resultString"])
+    monster_components = monster_components[:-1].split(",")
+    
+    
+    
+    # Beispiel-Funktion, die die übergebenen Parameter verarbeitet
+    result  = process_inputs_monster_parts(monster_CR,monster_type,monster_components)
+    options = type_selection
+    options2 = {}
+    return render_template("MonsterCalculator.html", result=result , options=options, options2=options2)
+
+
+@app.route("/mundaneitems", methods= ["POST"])
+def mundane_main():
+    options = region_selection
+    options2 ={}
+    return render_template("MundaneMaterial.html", options=options, options2 = options2)
+
+@app.route('/region', methods=["POST"])
+def region_select():
+    region = str(request.form["dropdown"])
+    selected_region = {}
+    selected_region[region] = region_selection[region]
+    options = selected_region
+    options2 = region_selection[region]
+    return render_template("MundaneMaterial.html", options=options, options2=options2 , region=region)
+
+@app.route('/region_calculate', methods=["POST"])
+def calculate_DC():
+    region = str(request.form["region"])
+    # Abrufen der ausgewählten Option aus dem Dropdown-Menü
+    material = str(request.form["material"])
+    dc = int(request.form["number"])
+    result = process_inputs_material(region,material,dc)
+    options2 = {}
+    options = region_selection
+    return render_template("MundaneMaterial.html", options=options, options2=options2 , result=result)
+
+
+#Functions needed for Calculations
+def process_inputs_monster_parts(monster_CR,monster_type, monster_components):
+    a = HarvestCalculator(monster_CR,monster_type, monster_components)
+   
+    
+    return a.calculation()
+
+def process_inputs_material(region,material,dc):
+    b = MundaneIngredients(region,material,dc)
+    return b.calculate()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
+    
